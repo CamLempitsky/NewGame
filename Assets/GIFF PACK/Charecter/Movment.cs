51ce29de573b7bc34653e036b3f9e096b0bf18a7
partyman <devin3527@gmail.com>
2023-09-14 19:23:00 -0700

    try

---
+++ Assets/GIFF PACK/Charecter/Movment.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public float speed;
+   private Rigidbody2D myRigidbody; 
+    private Vector2 change;
+    // Start is called before the first frame update
+    void Start() { myRigidbody = GetComponent<Rigidbody2D>();}
+
+    // Update is called once per frame
+    void Update()
+    { change = Vector2.zero;
+    change.x = Input.GetAxisRaw("Horizontal");
+     change.y = Input.GetAxisRaw("Vertical");
+     Debug.Log(change);
+        
+    }
+}
